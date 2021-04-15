c72441342e89f3a8a99b3f2e5fc3da4696bab574
gaurav kumar <49759621+Rgauravraj@users.noreply.github.com>
2021-04-15 21:03:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return "Hello, this is Gaurav !"
     return "Hello, this is a sample Python Web App running on Flask Framework!"
+    return "Welcome !"
 
